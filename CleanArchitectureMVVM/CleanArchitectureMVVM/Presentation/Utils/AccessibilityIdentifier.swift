728c138f65ce4ba3454555c479152d4b502d1b2e
Zizi_Kim <rlarjsdn3@naver.com>
2024-11-20 09:42:04 +0900

    commit

---
+++ CleanArchitectureMVVM/CleanArchitectureMVVM/Presentation/Utils/AccessibilityIdentifier.swift
@@ -0,0 +1,13 @@
+//
+//  AccessibilityIdentifier.swift
+//  CleanArchitectureMVVM
+//
+//  Created by 김건우 on 11/20/24.
+//
+
+import Foundation
+
+struct AccessibilityIdentifier {
+    static let movieDetailsView = "AccessibilityIdentifierMovieDetailsView"
+    static let searchField = "AccessibilityIdentifierSearchMovie"
+}
