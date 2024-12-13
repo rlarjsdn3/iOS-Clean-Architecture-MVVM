3f811ed04bd2dff2491acce68a35ba29b20c80a6
Zizi_Kim <rlarjsdn3@naver.com>
2024-12-13 10:08:20 +0900

    commit

---
+++ CleanArchitectureMVVM/Presentation/MovieScene/MoviesQueriesList/View/UIKit/Cells/MoviesQueriesItemCell.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesQueriesItemCell.swift
+//  CleanArchitectureMVVM
+//
+//  Created by 김건우 on 12/13/24.
+//
+
+import Foundation
---
+++ CleanArchitectureMVVM/Presentation/MovieScene/MoviesQueriesList/View/UIKit/MoviesQueriesTableViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesQueriesTableViewController.swift
+//  CleanArchitectureMVVM
+//
+//  Created by 김건우 on 12/13/24.
+//
+
+import Foundation
